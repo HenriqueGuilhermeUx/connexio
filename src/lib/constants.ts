76dfb9717e268ba70581610d234ad193690a9a55
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-21 14:26:34 -0300

    chore: version legal documents

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,6 @@ export const CONNEXIO = {
   cnpj: '61.920.356/0001-38',
   supportEmail: 'henriquecampos66@gmail.com',
   imageBucket: 'listing-images',
+  termsVersion: '2026-07-21',
+  privacyVersion: '2026-07-21',
 } as const;
