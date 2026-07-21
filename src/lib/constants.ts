3e19c8dcde0a274ef09307e6da7502dbdfd858f2
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-21 13:54:58 -0300

    chore: centralize Connexio constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const CONNEXIO = {
+  company: 'Alternative Ventures',
+  cnpj: '61.920.356/0001-38',
+  supportEmail: 'henriquecampos66@gmail.com',
+  imageBucket: 'listing-images',
+} as const;
